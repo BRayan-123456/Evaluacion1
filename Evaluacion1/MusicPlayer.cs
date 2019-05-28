2a901411d66ea9f859f15ef95fabdb733b333ce8
BRayan-123456 <41800589+BRayan-123456@users.noreply.github.com>
2019-05-27 22:26:14 -0500

    Initial commit

---
+++ Evaluacion1/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evaluacion1
+{
+    class MusicPlayer
+    {
+        public string Nombre { get; set; }
+        public string Genero { get; set; }
+        public string Album { get; set; }
+        public int TiemRepro { get; set; }
+
+
+
+        
+        
+    }
+}
---
+++ Evaluacion1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Evaluacion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+         
+            Console.WriteLine($" insertar (1)");
+            Console.WriteLine($" listar (2)");
+            
+            var resultado = Console.ReadLine();
+            List<MusicPlayer> musica = new List<MusicPlayer>();
+
+
+
+
+
+
+            if (resultado == "1")
+            {
+                MusicPlayer mus = new MusicPlayer();
+                Console.WriteLine($"Digite el nombre de cancion");
+                mus.Nombre = Console.ReadLine();
+
+                Console.WriteLine($"Digite el genero de la cancion");
+                mus.Genero = Console.ReadLine();
+
+                Console.WriteLine($"Digite el album de la cancion");
+                mus.Album = Console.ReadLine();
+
+                Console.WriteLine($"Digite el tiempo que dura la cancion");
+                mus.TiemRepro = int.Parse(Console.ReadLine());
+
+                musica.Add(mus);
+
+
+            }
+
+            Console.WriteLine($" insertar (1)");
+            Console.WriteLine($" listar (2)");
+            
+            resultado = Console.ReadLine();
+
+            if (resultado == "2")
+            {
+                foreach (var item in musica)
+                {
+                    Console.WriteLine($"el nombre:{ item.Nombre} genero:{item.Genero} Album:{item.Album} Tiempo reproduccion: {item.TiemRepro}");
+                }
+            }
+
+        }
+        
+    }
+}
